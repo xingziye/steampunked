fe8beedd1fef658f645cd8a9baba3dac3f8887f7
Ziye Xing <xingziye46@gmail.com>
2016-02-12 14:31:58 -0500

    initial

--- index.php
+++ index.php
@@ -11,8 +11,30 @@
 <head>
     <meta charset="UTF-8">
     <title>Steampunked</title>
+    <link href="project1.css" type="text/css" rel="stylesheet" />
 </head>
 <body>
-
+<form ...>
+    <div class="game">
+        <div class="row">
+            <div class="cell"></div><div class="cell"><img src="images/cap-e.png"></div><div class="cell"><img src="images/cap-e.png"></div>
+        </div>
+        <div class="row">
+            <div class="cell"><img src="images/cap-e.png"></div><div class="cell"><img src="images/cap-e.png"></div><div class="cell"><img src="images/cap-e.png"></div><div class="cell"><img src="images/cap-e.png"></div><div class="cell"><img src="images/cap-e.png"></div><div class="cell"><img src="images/cap-e.png"></div>
+        </div>
+        <div class="row">
+            <div class="cell"><img src="images/cap-e.png"></div><div class="cell"><img src="images/cap-e.png"></div><div class="cell"><img src="images/cap-e.png"></div><div class="cell"><img src="images/cap-e.png"></div><div class="cell"><img src="images/cap-e.png"></div><div class="cell"><img src="images/cap-e.png"></div>
+        </div>
+        <div class="row">
+            <div class="cell"><img src="images/cap-e.png"></div><div class="cell"><img src="images/cap-e.png"></div><div class="cell"><img src="images/cap-e.png"></div><div class="cell"><img src="images/cap-e.png"></div><div class="cell"><img src="images/cap-e.png"></div><div class="cell"><img src="images/cap-e.png"></div>
+        </div>
+        <div class="row">
+            <div class="cell"><img src="images/cap-e.png"></div><div class="cell"><img src="images/cap-e.png"></div><div class="cell"><img src="images/cap-e.png"></div><div class="cell"><img src="images/cap-e.png"></div><div class="cell"><img src="images/cap-e.png"></div><div class="cell"><img src="images/cap-e.png"></div>
+        </div>
+        <div class="row">
+            <div class="cell"><img src="images/cap-e.png"></div><div class="cell"><img src="images/cap-e.png"></div><div class="cell"><img src="images/cap-e.png"></div><div class="cell"><img src="images/cap-e.png"></div><div class="cell"><img src="images/cap-e.png"></div><div class="cell"><img src="images/cap-e.png"></div>
+        </div>
+    </div>
+</form>
 </body>
 </html>
