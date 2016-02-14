18c3a639e39e230d364c4b63fc4f3574f554c950
santor10 <santor10@msu.edu>
2016-02-14 00:49:08 -0500

    Finished View Tests

--- lib/Steampunked/View.php
+++ lib/Steampunked/View.php
@@ -11,5 +11,43 @@ namespace Steampunked;
 
 class View
 {
+    /**
+     * Constructor
+     * @param Steampunked $steampunked The Steakmpunked game object
+     */
+    public function __construct(Steampunked $steampunked) {
+        $this->game = $steampunked;
+    }
+
+    public function createGrid(){
+        $html = <<<HTML
+
+HTML;
+        return $html;
+    }
+
+    public function createButtons(){
+        $html = <<<HTML
+
+HTML;
+        return $html;
+    }
+
+    public function numRows(){
+        return $this->rows;
+    }
+
+    public function numCols(){
+        return $this->columns;
+    }
+
+    public function radioButtons(){
+        return $this->radio;
+    }
+
+    private $radio;
+    private $rows;
+    private $columns;
+    private $game;
 
 }
--- tests/tests/ViewTest.php
+++ tests/tests/ViewTest.php
@@ -9,11 +9,43 @@ require __DIR__ . "/../../vendor/autoload.php";
 
 class ViewTest extends \PHPUnit_Framework_TestCase
 {
-	public function test_construct() {
+	public function test_construct()
+	{
 		$steampunked = new Steampunked\Steampunked();
 		$view = new Steampunked\View($steampunked);
 
 		$this->assertInstanceOf('Steampunked\View', $view);
+	}
+
+	public function test_gridPresent()
+	{
+		$steampunked = new Steampunked\Steampunked();
+		$view = new Steampunked\View($steampunked);
+		$rows = $view->numRows(); // 6x6 grid
+		$columns = $view->numCols(); // 6x6 grid
+		$html = $view->createGrid();
+
+		$this->assertContains('<p><img src="images/title.png"></p>', $html); // beginning of view
+		$this->assertContains('<div class="row">', $html);
+		$this->assertContains('<div class="cell">', $html);
+		$this->assertEquals(6, $rows); //match 6x6 grid
+		$this->assertEquals(6, $columns); //match 6x6 grid
+	}
+
+	public function test_buttonsPresent()
+	{
+		$steampunked = new Steampunked\Steampunked();
+		$view = new Steampunked\View($steampunked);
+		$html = $view->createButtons();
+		$num_radio = $view->radioButtons();
+
+		$this->assertContains('<p class="option"><input type="button" name="rotate" value="Rotate"></p>', $html);
+		$this->assertContains('<p class="option"><input type="button" name="discard" value="Discard"></p>', $html);
+		$this->assertContains('<p class="option"><input type="button" name="open" value="Open Valve"></p>', $html);
+		$this->assertContains('<p class="option"><input type="button" name="giveup" value="Give Up"></p>', $html);
+		$this->assertContains('<input type="radio"', $html); ///assert that radio buttons exist
+		$this->assertEquals(5, $num_radio); //assert 5 radio buttons present
+
 	}
 }
 
