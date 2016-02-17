ec30ccf913dc47d55f6687be4c297367f85bfa94
jiashun <liujias3@msu.edu>
2016-02-17 18:04:04 -0500

    controller

--- tests/tests/ControllerTest.php
+++ tests/tests/ControllerTest.php
@@ -6,11 +6,40 @@
  * @brief Unit tests for the class 
  */
 require __DIR__ . "/../../vendor/autoload.php";
-
 class ControllerTest extends \PHPUnit_Framework_TestCase
 {
+    const SEED = 1422668587;
 	public function test_construct() {
+		$Steampunked = new Steampunked\Steampunked(self::SEED);
+		$controller = new Steampunked\Controller($Steampunked, array());
 
+		$this->assertInstanceOf('Steampunked\Controller', $controller);
+		$this->assertFalse($controller->isReset());
+		$this->assertEquals('game.php', $controller->getPage());
+	}
+	public function test_new() {
+		$Steampunked = new Steampunked\Steampunked(self::SEED);
+		$controller = new Steampunked\Controller($Steampunked, array('n' => ''));
+
+		$this->assertInstanceOf('Steampunked\Controller', $controller);
+		$this->assertTrue($controller->isReset());
+		$this->assertEquals('game.php', $controller->getPage());
+	}
+	public function test_move() {
+		$Steampunked = new Steampunked\Steampunked(self::SEED);
+		$controller = new Steampunked\Controller($Steampunked, array('n' => ''));
+
+		$this->assertFalse($controller->isReset());
+		$this->assertEquals('game.php', $controller->getPage());
+
+		$this->assertEquals(11, $$Steampunked->getCurrent()->getNdx());
+	}
+	public function test_post() {
+		$Steampunked = new Steampunked\Steampunked(self::SEED);
+		$controller = new Steampunked\Controller($Steampunked, array('n' => ''));
+
+		$this->assertTrue($controller->isReset());
+		$this->assertEquals('win.php', $controller->getPage());
 	}
 }
 
