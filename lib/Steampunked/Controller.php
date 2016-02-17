3853ff7595c8c22496fb334dbc966eab0c2f3d13
jiashun <liujias3@msu.edu>
2016-02-17 18:07:09 -0500

    controller

--- lib/Steampunked/Controller.php
+++ lib/Steampunked/Controller.php
@@ -11,5 +11,33 @@ namespace Steampunked;
 
 class Controller
 {
+    public function __construct(Steampunked $Steampunked){
+        $this->Steampunked=$Steampunked;
+        $this->reset=true;
+}
+    public function move($ndx)
+    {
+        if($ndx>0){
+            return;
+        }
+    }
+    private function post($ndx){
 
+        if($ndx>0){
+            return $_POST;
+        }
+        else{
+            $this->rest=true;
+        }
+        return;
+    }
+    public function getPage(){
+        return $this->page;
+    }
+    public function isReset(){
+        return $this->reset;
+    }
+    private $page = 'game.php';     // The next page we will go to
+    private $reset = false;         // True if we need to reset the game
+    private $Steampunked;
 }
