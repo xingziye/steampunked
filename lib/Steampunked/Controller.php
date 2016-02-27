b57e9e61cbb1d22139f06fedbb268d01478f523c
ljs <liujias3@msu.edu>
2016-02-27 17:24:20 -0500

    controller

--- lib/Steampunked/Controller.php
+++ lib/Steampunked/Controller.php
@@ -11,33 +11,89 @@ namespace Steampunked;
 
 class Controller
 {
-    public function __construct(Steampunked $Steampunked){
+    public function __construct(Steampunked $Steampunked, $post){
         $this->Steampunked=$Steampunked;
-        $this->reset=true;
-}
-    public function move($ndx)
-    {
-        if($ndx>0){
-            return;
+        if(isset($post['add'])){
+            $this->action='add';
+            $this->addimage($post['add']);
         }
-    }
-    private function post($ndx){
+        else if(isset($post['rotate'])){
+            $this->action='rotate';
+            $this->rotate($post['rotate']);
+        }
+        else if(isset($post['discard'])){
+            $this->action='discard';
+            $this->discard($post['discard']);
+        }
+        else if(isset($post['giveup'])){
+            $this->action='giveup';
+            $this->giveup($post['giveup']);
+        }
 
-        if($ndx>0){
-            return $_POST;
-        }
-        if ($this->Steampunked->post($ndx)){
-            $this->rest=true;
-        }
-        return;
+}
+    public function addimage($image,$row,$colum){
+        $this->action='add';
+        $this->move();
     }
+    public function rotate($image){
+        $this->action='rotate';
+        $this->move();
+    }
+    public function discard(){
+        $this->action='discard';
+        $this->move();
+    }
+    public function giveup($image,$row,$colum){
+        $this->action='giveup';
+        $this->move();
+    }
+    public function move()
+    {
+        if($this->action='giveup'){
+            $this->action=null;
+            $this->image=null;
+            $this->page='win.php';
+            //make it win no matter win or lose
+        }
+        else if($this->action == 'add')
+        {
+            //check if adding piece wins
+            //if(piece placed wins game){$this->page = win.php;}
+            //change players
+            $this->page = 'Steampunked.php';
+        }
+
+        else if($this->action == 'rotate')
+        {
+            $this->page = 'Steampunked.php';
+        }
+
+        else if($this->action == 'discard')
+        {
+            $this->piece = null;
+            //change players
+            $this->page = 'Steampunked.php';
+        }
+
+    }
+
     public function getPage(){
         return $this->page;
     }
     public function isReset(){
         return $this->reset;
     }
-    private $page = 'game.php';     // The next page we will go to
-    private $reset = false;         // True if we need to reset the game
+    public function getAction()
+    {
+        return $this->action;
+    }
+
+    public function getImage()
+    {
+        return $this->image;
+    }
+    private $page = 'Steampunked.php';     // The next page we will go to
+    private $image;
     private $Steampunked;
+    private $action;
 }
