25d86e508b3e22d8f9c9e468fb7a89dd768d2680
Ziye Xing <xingziye46@gmail.com>
2016-02-28 02:27:23 -0500

    welcome page doesn't have game object yet

--- index.php
+++ index.php
@@ -1,8 +1,3 @@
-<?php
-require 'lib/game.inc.php';
-$view = new Steampunked\View($steampunked);
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -11,8 +6,35 @@ $view = new Steampunked\View($steampunked);
     <link href="project1.css" type="text/css" rel="stylesheet" />
 </head>
 <body>
-    <?php echo $view->startScreenButtons() ; ?>
-
-
+<div class="screen">
+    <p><img src="images/title.png" alt="Steampunked Logo"></p>
+    <form method="post" action="game-post.php">
+        <fieldset>
+            <legend>Game Preferences</legend>
+            <p>
+                <label for="player1"> Player 1 Name:</label>
+                <input type="text" name="player1" id="player1">
+            </p>
+            <br>
+            <p>
+                <label for="player2"> Player 2 Name:</label>
+                <input type="text" name="player2" id="player2">
+            </p>
+            <br>
+            <p>
+                <label for="6x6">6x6</label>
+                <input type="radio" name="gamesize" id="6x6" value="6">
+                <label for="10x10">10x10</label>
+                <input type="radio" name="gamesize" id="10x10" value="10">
+                <label for="20x20">20x20</label>
+                <input type="radio" name="gamesize" id="20x20" value="20">
+            </p>
+            <br>
+            <p>
+                <input type="submit">
+            </p>
+        </fieldset>
+    </form>
+</div>
 </body>
 </html>
