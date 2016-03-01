9d056bc539ffe6324c1b68e35648949b1a2677ef
santor10 <santor10@msu.edu>
2016-02-29 20:26:53 -0500

    Added function to create index in View

--- lib/Steampunked/View.php
+++ lib/Steampunked/View.php
@@ -210,6 +210,44 @@ HTML;
         }
     }
 
+    public function createStartPage(){
+        $html = <<<HTML
+        <div class="screen">
+    <p><img src="images/title.png" alt="Steampunked Logo"></p>
+    <form method="post" action="game-post.php">
+        <fieldset>
+            <legend>Game Preferences</legend>
+            <p>
+                <label for="player1"> Player 1 Name:</label>
+                <input type="text" name="player1" id="player1">
+            </p>
+            <br>
+            <p>
+                <label for="player2"> Player 2 Name:</label>
+                <input type="text" name="player2" id="player2">
+            </p>
+            <br>
+            <p>
+                <label for="6x6">6x6</label>
+                <input type="radio" name="gamesize" id="6x6" value="6" checked="checked>
+                <label for="10x10">10x10</label>
+                <input type="radio" name="gamesize" id="10x10" value="10">
+                <label for="20x20">20x20</label>
+                <input type="radio" name="gamesize" id="20x20" value="20">
+            </p>
+            <br>
+            <p>
+                <input type="submit">
+            </p>
+        </fieldset>
+    </form>
+</div>
+
+HTML;
+
+        return $html;
+    }
+
 
     private $game;
 }
